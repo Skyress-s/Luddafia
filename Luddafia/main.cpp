0e60c4049d3fa9e90e59ee2c820e05faa8dbeea7
Skyress-s <mathmoe64@gmail.com>
2021-09-06 23:20:32 +0200

    determine how manye mafias, insert names, removed original player count question

--- Luddafia/main.cpp
+++ Luddafia/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
+void CinClear();
 
 class Player
 {
@@ -41,15 +44,58 @@ Mafia::~Mafia()
 {
 }
 
+
+
 int main() {
+	//the name of the players
+	vector<string> names{};
 
-	Player test;
-	bool woah = test.isAlive;
-	std::cout << woah << endl;
+	bool accepted = false;
+	while (accepted == false)
+	{
+		string answer{};
+		cout << " Please input name / 'q' to exit loop : ";
+		getline(cin, answer);
+		//cin >> answer; // 
+		//CinClear();  //why cant i have this ? ? ? !? !? !? !?
+		
 
-	Mafia maf;
-	bool Bwaf = maf.isAlive;
-	std::cout << Bwaf;
+		//exit the loop when you have selected all the players you want
+		if (answer[0] == 'q' && answer.size() == 1)
+		{
+			break;
+		}
+
+		names.push_back(answer);
+
+	}
+
+
+	//determines how many mafias
+
+	int personsPerMafia = 4;
+	if (personsPerMafia > names.size()) // ensures there always is atleast ONE mafia ^^
+	{
+		personsPerMafia = names.size();
+	}
+
+	int n = floor((float(names.size()) / personsPerMafia));
+	cout << "Num of mafias : " << n << endl;
+
+
+
+
+
+	//debug print
+	for (int i = 0; i < names.size(); i++)
+	{
+		cout << names[i] << endl;
+	}
+
+}
+
+void CinClear() {
+	std::cin.clear(); // clears the cin buffer for errors
+	std::cin.ignore(32767, '\n');
 	
-
 }
